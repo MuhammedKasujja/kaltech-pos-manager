40ef6bab3dda6a5b7b358709dc98dac64dd224de
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-06-14 20:45:45 +0300

    feat: added seeder to create users

--- prisma/migrations/20250614153303_y/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `accountKey` on the `SyncDevice` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `SyncDevice` DROP COLUMN `accountKey`;
