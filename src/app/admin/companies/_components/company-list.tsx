2aad1b30fda46526cf68d1f853634158a1993d3e
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-06-14 15:13:19 +0300

    fix: show company list

--- src/app/admin/companies/_components/company-list.tsx
+++ src/app/admin/companies/_components/company-list.tsx
@@ -2,15 +2,14 @@
 
 import { useCompanies } from "@/lib/swr/use-companies";
 
-export default function CompanyList() {
+export function CompanyList() {
   const { companies, error } = useCompanies();
   if (error) return <div>{`${error}`}</div>;
 
   return (
     <div>
-        flkfkl
       {companies?.map((company) => (
-        <div key={company.phone}>{company.name}</div>
+        <div key={company.phone}>{company.name} {company.phone}</div>
       ))}
     </div>
   );
