b626ea1429e50cbcae102c2d93313e6a1504028c
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-06-19 11:45:03 +0300

    feat: added app logo

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -70,7 +70,7 @@ export function LoginForm({
           </Form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="/placeholder.svg"
+              src="/logo.png"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
