7ce78dcb787702805021118b017b52aba33aa1c0
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-09-14 14:37:54 +0300

    feat: check password equality

--- src/features/company/schemas/index.ts
+++ src/features/company/schemas/index.ts
@@ -1,9 +1,14 @@
 import { z } from "zod";
 
-export const changePasswordSchema = z.object({
-  accountKey: z.string().min(8).trim(),
-  password: z.string().min(8).trim(),
-  confirmPassword: z.string().min(8).trim(),
-});
+export const changePasswordSchema = z
+  .object({
+    accountKey: z.string().min(8).trim(),
+    password: z.string().min(8).trim(),
+    confirmPassword: z.string().min(8).trim(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 export type ChangeAdminPasswordType = z.infer<typeof changePasswordSchema>;
