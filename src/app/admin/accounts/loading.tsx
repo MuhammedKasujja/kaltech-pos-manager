2bd167748c310cf2f2731d02be257f1310f351b8
MuhammedKasujja <al.kasmud.2@gmail.com>
2026-01-09 17:24:44 +0300

    fix: using data table loading skeleton for accounts loading.tsx

--- src/app/admin/accounts/loading.tsx
+++ src/app/admin/accounts/loading.tsx
@@ -1,9 +1,23 @@
-import { LoadingShimmer } from "@/components/loading-shimmer";
+import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton";
+import { columns } from "./columns";
 
 export default function Loading() {
   return (
     <div className="md:gap-6 md:p-6 space-y-6">
-      <LoadingShimmer />
+      <DataTableSkeleton
+        columnCount={columns.length}
+        filterCount={1}
+        cellWidths={[
+          "10rem",
+          "10rem",
+          "15rem",
+          "6rem",
+          "10rem",
+          "10rem",
+          "6rem",
+        ]}
+        shrinkZero
+      />
     </div>
   );
 }
